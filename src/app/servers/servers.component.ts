4e7313628f5b4732c213cda024be4655d69d2ed9
Noura Moubtassim <31420260+nouraSt@users.noreply.github.com>
2023-08-14 18:22:40 +0100

    bindable props and events

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServers = false;
+  serversCreationStatus="no server was created"
 
-  constructor() { }
+  constructor() { 
+    setTimeout(
+      ()=>{
+        this.allowNewServers = true;
+      }, 2000
+    );
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+   this.serversCreationStatus = 'server was created';
+
   }
 
 }
