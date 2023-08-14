49933e554e5324a0065d9b9d4ed86c7df699bb32
Noura Moubtassim <31420260+nouraSt@users.noreply.github.com>
2023-08-14 17:26:39 +0100

    string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,5 +7,9 @@ import {Component} from '@angular/core';
 
 })
 export class ServerComponent {
-
+    serverId: number = 10;
+    serverStatus: string = "hello";
+    getServerStatus(){
+      return this.serverStatus;
+    }
 }
